3e324d3ae9ba38574535dc19d03fdae281fd2470
Soad Mohamed <soadm40@yahoo.com>
2024-04-24 14:28:51 +0200

    first change

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('soad', function () {
+    return 'welcome to my first web page';
+});
+
+Route::get('soad/{id}', function ($id) {
+    return 'this is  a parameter ' . $id ;
+});
